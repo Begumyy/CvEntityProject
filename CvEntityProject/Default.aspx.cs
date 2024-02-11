c370b7f260b94235985cf5e70bcafbff9bfa9bfc
Begüm Yünkül <yunkulbegum@gmail.com>
2024-02-11 19:11:54 +0300

    yeni template eklendi.

--- CvEntityProject/Default.aspx.cs
+++ CvEntityProject/Default.aspx.cs
@@ -12,12 +12,7 @@ namespace CvEntityProject
         DbCvEntityEntities db= new DbCvEntityEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
-            Repeater1.DataSource = db.ABOUTME.ToList();
-            Repeater1.DataBind();
-            Repeater2.DataSource = db.ABOUTME.ToList();
-            Repeater2.DataBind();
-            Repeater3.DataSource = db.ABOUTME.ToList();
-            Repeater3.DataBind();
+           
         }
     }
 }
