a93dea39e36fa6820ffda815431e9c2921fad458
Begüm Yünkül <yunkulbegum@gmail.com>
2024-02-13 15:26:47 +0300

    admin panelinde mesajlar görüntülendi.

---
+++ CvEntityProject/MessageDetail.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntityProject
+{
+    public partial class MessageDetail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DbCvEntityEntities1 db= new DbCvEntityEntities1();
+            int x = int.Parse(Request.QueryString["ID"]);
+            var mesaj = db.CONTACT.Find(x);
+            TxtNameSurname.Text = mesaj.NAMESURNAME;
+            TxtMail.Text = mesaj.MAIL;
+            TxtSubject.Text = mesaj.SUBJECT;
+            TxtMessage.Text = mesaj.MESSAGE;
+        }
+    }
+}
